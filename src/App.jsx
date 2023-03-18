dc0c74ce3655dd8dc7e45f31d4703757fc26fe5d
Thomson152 <tomsinonyedikachi@gmail.com>
2023-03-18 20:37:57 +0100

    Added my project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/** @format */
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Experience, Hero, Navbar,   } from "./components";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <About/>
+      <Experience/>
+    </div>
+  </BrowserRouter>
+  );
+}
+
+export default App;
